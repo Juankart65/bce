e1e33f190aefebb8dfaeb2e19694d0ac0d5fab69
Juankart65 <104481229+Juankart65@users.noreply.github.com>
2023-10-24 00:58:08 -0500

    added background image

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <>
       <main>
         <Nav />
+        
       </main>
     </>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
 
 // 1. import `NextUIProvider` component
 import { NextUIProvider } from "@nextui-org/react";
